2f80b2bce22af7eefaf1576d707f040deb510e6c
DaviFarias7 <davic.farias7@gmail.com>
2023-09-13 17:51:59 -0300

    create transaction entity

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.picpaysimplificado.repositories;
+
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByDocument(String document);
+
+    Optional<User> findUserById(Long aLong);
+}
