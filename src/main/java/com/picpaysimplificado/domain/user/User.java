3e4ae190f81f94f66e554f8df0faf7532df83b23
DaviFarias7 <davic.farias7@gmail.com>
2023-09-13 22:49:19 -0300

    create User, UserDTO, UserService, UserController

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
 
@@ -35,4 +34,15 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userTYpe;
+
+    public User(UserDTO data) {
+       this.firstName = data.firstName();
+       this.lastName = data.lastName();
+       this.document = data.document();
+       this.email = data.email();
+       this.password = data.password();
+       this.balance = data.balance();
+       this.userTYpe = data.userType();
+
+    }
 }
--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -2,11 +2,13 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -20,7 +22,25 @@ public class UserService {
         }
 
         if (sender.getBalance().compareTo(amount) < 0){
-            throw  new Exception("Usuário não tem saldo");
+            throw  new Exception("Saldo insuficiente");
         }
+    }
+
+    public User findUserById(Long id) throws Exception{
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrato"));
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+      return this.repository.findAll();
     }
 }
