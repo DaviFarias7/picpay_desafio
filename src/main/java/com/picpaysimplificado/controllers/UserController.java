c2bbb9652929618fbe4b95613364c59615a976b8
DaviFarias7 <davic.farias7@gmail.com>
2023-09-14 13:58:45 -0300

    create TransactionController, UserController

--- src/main/java/com/picpaysimplificado/controllers/UserController.java
+++ src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users")
 public class UserController {
 
     @Autowired
